fa57ee7400753557da621f19b7c6a9044c205c8a
Jiwon Jeong <jiwonjeong414@gmail.com>
2025-11-23 17:05:58 -0500

    Create board.mli

---
+++ lib/model/board.mli
@@ -0,0 +1,26 @@
+(** Game board - tile grid *)
+
+(** Type representing different tiles on the game board *)
+type tile =
+  | Soil of Crop.crop_instance option
+  | Shop  
+  | Path  
+
+(** Type representing the game board as a 2D array of tiles *)
+type board = tile array array
+
+(** [create_board width height] creates a new game board. *)
+val create_board : int -> int -> board
+
+(** [get_tile board x y] returns [Some tile] at position [(x, y)] if the
+    position is within bounds, or [None] otherwise. *)
+val get_tile : board -> int -> int -> tile option
+
+(** [set_tile board x y tile] sets the tile at position [(x, y)] to [tile].
+    Does nothing if the position is out of bounds. *)
+val set_tile : board -> int -> int -> tile -> unit
+
+(** [get_facing_tile board player] returns a tuple [(x, y, tile_opt)] where
+    [(x, y)] is the position of the tile the player is facing, and [tile_opt]
+    is [Some tile] if that position is valid, or [None] if out of bounds. *)
+val get_facing_tile : board -> Player.player -> int * int * tile option
